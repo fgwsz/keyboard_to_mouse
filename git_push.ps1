0fb222936c51c259af3ccb1d727935306924776c
fgwsz <3191000458@qq.com>
2023-07-03 00:41:54 +0800

    清理build文件

--- git_push.ps1
+++ git_push.ps1
@@ -1,9 +1,11 @@
 git add include/*
 git add src/*
 git add CMakeLists.txt
-git add *.ps1
-git add *.exe
+git add keyboard_to_mouse.exe
 git add README.md
+git add msvc_build.ps1
+git add mingw_build.ps1
+git add git_push.ps1
 echo "input commit info:"
 $commit_info=Read-Host
 git commit -m $commit_info

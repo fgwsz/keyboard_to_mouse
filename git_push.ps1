9e8d21afb845ecafdee9071672478e670d3a5087
fgwsz <3191000458@qq.com>
2023-10-01 06:56:57 +0800

    优化提交git_push

--- git_push.ps1
+++ git_push.ps1
@@ -6,7 +6,6 @@ git add README.md
 git add msvc_build.ps1
 git add mingw_build.ps1
 git add git_push.ps1
-echo "input commit info:"
-$commit_info=Read-Host
+$commit_info=Read-Host -Prompt "input commit info"
 git commit -m $commit_info
 git push
